255783dfe9d238e5ed78f0458a9118dadd51bd05
Maui89 <54687718+Maui89@users.noreply.github.com>
2019-08-29 22:55:53 +0200

    Create webserver_led_norefresh.ino

---
+++ webserver_led_norefresh.ino
@@ -0,0 +1,100 @@
+/*
+ * ESP32 AJAX Demo
+ * Updates and Gets data from webpage without page refresh
+ * https://circuits4you.com
+ */
+#include <WiFi.h>
+#include <WiFiClient.h>
+#include <WebServer.h>
+
+#include "index.h"  //Web page header file
+
+#define LED 21 
+
+WebServer server(80);
+
+//Enter your SSID and PASSWORD
+const char* ssid = "ASUS";
+const char* password = "Maui2007-";
+
+//===============================================================
+// This routine is executed when you open its IP in browser
+//===============================================================
+void handleRoot() {
+ String s = MAIN_page; //Read HTML contents
+ server.send(200, "text/html", s); //Send web page
+}
+ 
+void handleADC() {
+ int a = analogRead(34);
+ String adcValue = String(a);
+ 
+ server.send(200, "text/plane", adcValue); //Send ADC value only to client ajax request
+}
+void handleLED() {
+ String ledState = "OFF";
+ String t_state = server.arg("LEDstate"); //Refer  xhttp.open("GET", "setLED?LEDstate="+led, true);
+ Serial.println(t_state);
+ if(t_state == "0")
+ {
+  digitalWrite(LED,LOW); //LED OFF
+  ledState = "OFF"; //Feedback parameter
+ }
+ else
+ {
+  digitalWrite(LED,HIGH); //LED ON
+  ledState = "ON"; //Feedback parameter 
+ }
+ 
+ server.send(200, "text/plane", ledState); //Send web page
+}
+//===============================================================
+// Setup
+//===============================================================
+
+void setup(void){
+  Serial.begin(115200);
+  Serial.println();
+  Serial.println("Booting Sketch...");
+
+/*
+//ESP32 As access point
+  WiFi.mode(WIFI_AP); //Access Point mode
+  WiFi.softAP(ssid, password);
+*/
+//ESP32 connects to your wifi -----------------------------------
+  WiFi.mode(WIFI_STA); //Connectto your wifi
+  WiFi.begin(ssid, password);
+
+  Serial.println("Connecting to ");
+  Serial.print(ssid);
+
+  //Wait for WiFi to connect
+  while(WiFi.waitForConnectResult() != WL_CONNECTED){      
+      Serial.print(".");
+    }
+    
+  //If connection successful show IP address in serial monitor
+  Serial.println("");
+  Serial.print("Connected to ");
+  Serial.println(ssid);
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());  //IP address assigned to your ESP
+//----------------------------------------------------------------
+ 
+  server.on("/", handleRoot);      //This is display page
+  server.on("/readADC", handleADC);//To get update of ADC Value only
+  server.on("/setLED", handleLED);
+ 
+  server.begin();                  //Start server
+  Serial.println("HTTP server started");
+  pinMode(LED, OUTPUT);
+}
+
+//===============================================================
+// This routine is executed when you open its IP in browser
+//===============================================================
+void loop(void){
+  server.handleClient();
+  delay(1);
+}
